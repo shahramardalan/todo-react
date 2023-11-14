667e9b11c25cc9cd875c0f634c93d51302455c75
shahram <ardalan.shahram@gmail.com>
2023-11-14 23:25:17 +0330

    recreate deleteTask and toggleTask function by filter and map

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Todo from "./components/Todo";
 function uuidv4() {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     const r = (Math.random() * 16) | 0,
-      v = c == "x" ? r : (r & 0x3) | 0x8;
+      v = c === "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 }
@@ -19,20 +19,21 @@ function App() {
     setList(newList);
   };
 
-  const deleteTask = (index) => {
-    const newList = [...list];
-    newList.splice(index, 1);
+  const deleteTask = (id) => {
+    const newList = list.filter((todo) => {
+      return todo.id !== id;
+    });
+    console.log(newList)
     setList(newList);
-    console.log(newList);
   };
 
-  const toggleTask = (index) => {
-    const newList = [...list];
-    if (newList[index].status == list[index].status) {
-      newList[index].status = !list[index].status;
-      setList(newList);
-      console.log(newList);
-    }
+  const toggleTask = (id) => {
+    const newList = list.map((todo) => {
+      return todo.id === id
+        ? { id: todo.id, title: todo.title, status: !todo.status }
+        : { ...todo };
+    });
+    setList(newList);
   };
 
   return (
@@ -40,13 +41,13 @@ function App() {
       <div className="App">
         <TodoForm addTask={addTask} />
 
-        {list.map((todo, index) => (
+        {list.map((todo) => (
           <Todo
-            key={index}
+            key={todo.id}
             title={todo.title}
             status={todo.status}
-            deleteTask={() => deleteTask(index)}
-            toggleTask={() => toggleTask(index)}
+            deleteTask={() => deleteTask(todo.id)}
+            toggleTask={() => toggleTask(todo.id)}
           />
         ))}
       </div>
